d073a1e61b9b0c9095348e835bff2377d25f04ea
Sintiya <124550908+sintiyaaa73@users.noreply.github.com>
2024-05-05 18:58:54 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pencatatan Keuangan Harian
+Aplikasi pencatatan keuangan harian ini memiliki tampilan yang sederhana dan fitur yang mampu berfungsi dengan baik.
+
+To access. Example http://localhost/keuangan
+
+## Admin Account
+|  Username | Password |
+|:---------:|---------:|
+|  admin    |  admin   |
+
+## Sistem Requirement
+- Database MySQL
+- XAMPP / PHP 5.6
+- Server : XAMPP
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php 
+    session_start();
+    require "function/functions.php";
+    
+    if ( isset($_SESSION["login"]) ) {
+        header("Location: dashboard");
+        exit;
+    } elseif(isset($_COOKIE['login'])) {
+        header("Location: dashboard");
+        exit;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="img/icon.png">
+    <title>Catatan Pengeluaran Harian</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        type="text/css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/modal.css">
+    <style>
+        .parallax {
+            background: url(img/bg3.jpg);
+            background-attachment: fixed;
+            /* background-size: cover; */
+            background-repeat: no-repeat;
+        }   
+    </style>
+</head>
+
+<body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top main-nav" id="mainNav">
+        <div class="container">
+            <a class="js-scroll-trigger" href="#page-top">
+                <img src="img/logo.png" width="20px" style="margin-right: 10px; margin-bottom: 2px;">
+            </a>
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">Catata pengeluaran harian</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="login" class="nav-link">Sign in</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Navigation -->
+
+    <!-- header -->
+    <header id="home" class="text-light parallax">
+        <div class="container konten">
+            <h1 style="font-size: 36pt;">Welcome to Catatan Pengeluaran Harian</h1>
+            <p class="lead" style="font-size: 16pt;">Aplikasi pencatatan keuangan harian</p>
+            <a href="login" class="btn btn-outline-light button">Get Started</a>
+        </div>
+    </header>
+    <!-- header -->
+
+    <!-- js utama -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- js scrolling -->
+    <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
